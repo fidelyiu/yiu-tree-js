9595943d65b6a05fa24290f82b3eb56239982d00
Fidel Yiu <fidel-yiu@qq.com>
2023-02-09 10:51:57 +0800

    chore: 添加单元测试

--- packages/yiu-tree/test/get/get-filter-by-search.test.ts
+++ packages/yiu-tree/test/get/get-filter-by-search.test.ts
@@ -2,11 +2,40 @@ import { getFilterBySearch } from '../../src'
 import type { TreeFilterOpt } from '../../src/type'
 
 describe('过滤测试', () => {
-    test('父节点匹配-子节点不匹配', () => {
+    describe('父节点匹配-子节点不匹配', () => {
         const matchOpt: TreeFilterOpt = {
             parentMatch: true,
             childrenMatch: false,
         }
-        expect(getFilterBySearch([], () => false, matchOpt)).toStrictEqual([])
+        test('测试1', () => {
+            type TreeNode = {
+                id: string | number
+                age?: number
+                children?: TreeNode[]
+            }
+            const tree: TreeNode[] = [
+                {
+                    id: 1,
+                    children: [
+                        {
+                            id: '1-1',
+                            children: [{ id: '1-1-1' }, { id: '1-1-2' }],
+                        },
+                        { id: '1-2' },
+                        { id: '1-3' },
+                    ],
+                },
+                { id: 2, children: [{ id: '2-1' }] },
+                { id: 3 },
+                { id: 4 },
+            ]
+            expect(
+                getFilterBySearch<TreeNode>(
+                    tree,
+                    (node) => node.id === 2,
+                    matchOpt
+                )
+            ).toStrictEqual([{ id: 2, children: [{ id: '2-1' }] }])
+        })
     })
 })
