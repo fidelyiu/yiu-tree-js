43e648438070dbf1a83e9224a928000b03699739
Fidel Yiu <fidel-yiu@qq.com>
2023-02-09 11:21:52 +0800

    chore: 添加单元测试

--- packages/yiu-tree/test/get/get-filter-by-search.test.ts
+++ packages/yiu-tree/test/get/get-filter-by-search.test.ts
@@ -37,5 +37,55 @@ describe('过滤测试', () => {
                 )
             ).toStrictEqual([{ id: 2, children: [{ id: '2-1' }] }])
         })
+        test('测试2', () => {
+            type TreeNode = {
+                id: string | number
+                name: string
+                age?: number
+                children?: TreeNode[]
+            }
+            const tree: TreeNode[] = [
+                {
+                    id: 1,
+                    name: 'name-1',
+                    children: [
+                        {
+                            id: '1-1',
+                            name: 'name@1-1',
+                            children: [
+                                { id: '1-1-1', name: 'name@1-1-1' },
+                                { id: '1-1-2', name: 'name@1-1-2' },
+                            ],
+                        },
+                        { id: '1-2', name: 'name@1-2' },
+                        { id: '1-3', name: 'name@1-3' },
+                    ],
+                },
+                {
+                    id: 2,
+                    name: 'name-2',
+                    children: [{ id: '2-1', name: 'name@2-1' }],
+                },
+                { id: 3, name: 'name-3' },
+                { id: 4, name: 'name-4' },
+            ]
+
+            expect(
+                getFilterBySearch<TreeNode>(
+                    tree,
+                    (node) =>
+                        ['name-2', 'name-3'].includes(node.name) &&
+                        node.name !== 'name@2-1',
+                    matchOpt
+                )
+            ).toStrictEqual([
+                {
+                    id: 2,
+                    name: 'name-2',
+                    children: [{ id: '2-1', name: 'name@2-1' }],
+                },
+                { id: 3, name: 'name-3', children: [] },
+            ])
+        })
     })
 })
