bb9fd90a63aa34a77f78eadd2ab934c05373c857
Fidel Yiu <fidel-yiu@qq.com>
2023-03-16 20:25:16 +0800

    chore: 修改树摇

--- packages/yiu-tree/src/get/get-shake-by-search.ts
+++ packages/yiu-tree/src/get/get-shake-by-search.ts
@@ -11,69 +11,61 @@ function _getShakeBySearch<T>(
     nodePath: Array<T>,
     opt: TreeBaseOpt<T>
 ): { data: T[]; hasShake: boolean } {
-    let shakeResult = false
     let hasShake = false
     const result: T[] = []
     if (!Array.isArray(treeData) || !treeData.length) {
-        return {
-            data: result,
-            hasShake,
-        }
+        return { data: result, hasShake }
     }
-    do {
-        hasShake = false
-        const treeDataLen = treeData.length
-        for (let i = 0; i < treeDataLen; i++) {
-            const treeNode = treeData[i]
-            const currentPath = [...nodePath, treeNode]
-            const children = getTreePropsValue<T>(treeNode, 'children', opt)
-            const childrenIsArr = Array.isArray(children)
-            const childrenLen = childrenIsArr ? children.length : 0
-            if (childrenLen !== 0) {
-                const shakeChildren = _getShakeBySearch<T>(
-                    children,
-                    scFunc,
-                    currentLevel + 1,
-                    treeNode,
-                    currentPath,
-                    opt
-                )
+    hasShake = false
+    const treeDataLen = treeData.length
+    for (let i = 0; i < treeDataLen; i++) {
+        const treeNode = treeData[i]
+        const currentPath = [...nodePath, treeNode]
+        const children = getTreePropsValue<T>(treeNode, 'children', opt)
+        const childrenIsArr = Array.isArray(children)
+        const childrenLen = childrenIsArr ? children.length : 0
+        if (childrenLen !== 0) {
+            const childrenShakeResult = _getShakeBySearch<T>(
+                children,
+                scFunc,
+                currentLevel + 1,
+                treeNode,
+                currentPath,
+                opt
+            )
 
-                if (shakeChildren.hasShake) {
-                    shakeResult = true
-                    hasShake = true
-                }
-
-                setTreePropsValue<T>(
-                    treeNode,
-                    'children',
-                    shakeChildren.data,
-                    opt
-                )
-            }
-
-            // 当前节点是否匹配
-            const currentMatch = !!scFunc(treeNode, {
-                level: currentLevel + 1,
-                index: i,
-                isLeaf: !childrenLen,
-                isFirst: i === 0,
-                isLast: i === treeDataLen - 1,
-                parent,
-                path: currentPath,
-                parentPath: nodePath,
-            })
-
-            if (currentMatch) {
-                result.push(treeNode)
-            } else {
-                shakeResult = true
+            if (childrenShakeResult.hasShake) {
                 hasShake = true
             }
+
+            setTreePropsValue<T>(
+                treeNode,
+                'children',
+                childrenShakeResult.data,
+                opt
+            )
         }
-    } while (hasShake)
+
+        // 当前节点是否匹配
+        const currentMatch = scFunc(treeNode, {
+            level: currentLevel + 1,
+            index: i,
+            isLeaf: !childrenLen,
+            isFirst: i === 0,
+            isLast: i === treeDataLen - 1,
+            parent,
+            path: currentPath,
+            parentPath: nodePath,
+        })
+
+        if (currentMatch) {
+            result.push(treeNode)
+        } else {
+            hasShake = true
+        }
+    }
     treeData = result
-    return { data: result, hasShake: shakeResult }
+    return { data: result, hasShake }
 }
 
 export default function getShakeBySearch<T>(
@@ -86,6 +78,19 @@ export default function getShakeBySearch<T>(
     const baseOpt: TreeBaseOpt<T> = Object.assign(defOpt, opt)
     const deepData = getDeepTree<T>(treeData, baseOpt, true)
     if (!Array.isArray(deepData) || !deepData.length) return []
-    return _getShakeBySearch<T>(deepData, scFunc, 0, undefined, [], baseOpt)
-        .data
+    let result = deepData
+    let hasShake = false
+    do {
+        const shakeResult = _getShakeBySearch<T>(
+            deepData,
+            scFunc,
+            0,
+            undefined,
+            [],
+            baseOpt
+        )
+        result = shakeResult.data
+        hasShake = shakeResult.hasShake
+    } while (hasShake)
+    return result
 }
