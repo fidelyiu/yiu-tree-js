759f4061211c2c10284e73fbc4dc8974a80fe33b
Fidel Yiu <fidel-yiu@qq.com>
2023-03-20 10:30:35 +0800

    chore: 添加文档

---
+++ packages/web-site/docs/.vitepress/config.ts
@@ -0,0 +1,80 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  base: "/yiu-tree-js/",
+  title: "YiuTree",
+  description: "A js library for manipulating trees.",
+  cleanUrls: true,
+  lastUpdated: true,
+  themeConfig: {
+    siteTitle: "YiuTree",
+    outline: "deep",
+    outlineTitle: "本文内容",
+    socialLinks: [
+      { icon: "github", link: "https://github.com/fidelyiu/yiu-tree-js" },
+    ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2019-present Fidel Yiu",
+    },
+    nav: [
+      { text: "指引", link: "/guide/index.md" },
+      { text: "工具", link: "/tool/get/get-filter-by-search/index.md" },
+    ],
+    sidebar: {
+      "/tool/": [
+        {
+          text: "Get",
+          items: [
+            {
+              text: "get-filter-by-search",
+              link: "/tool/get/get-filter-by-search/index.md",
+            },
+            {
+              text: "get-flat-list-by-tree",
+              link: "/tool/get/get-flat-list-by-tree/index.md",
+            },
+            {
+              text: "get-leaf-node-by-list",
+              link: "/tool/get/get-leaf-node-by-list/index.md",
+            },
+            { text: "get-max-level", link: "/tool/get/get-max-level/index.md" },
+            {
+              text: "get-one-node-by-search",
+              link: "/tool/get/get-one-node-by-search/index.md",
+            },
+            {
+              text: "get-one-node-path-by-search",
+              link: "/tool/get/get-one-node-path-by-search/index.md",
+            },
+            {
+              text: "get-shake-by-search",
+              link: "/tool/get/get-shake-by-search/index.md",
+            },
+            {
+              text: "get-tree-by-list",
+              link: "/tool/get/get-tree-by-list/index.md",
+            },
+            {
+              text: "get-tree-by-list-simple",
+              link: "/tool/get/get-tree-by-list-simple/index.md",
+            },
+          ],
+        },
+        {
+          text: "Has",
+          items: [
+            { text: "has-by-search", link: "/tool/has/has-by-search/index.md" },
+          ],
+        },
+        {
+          text: "Op",
+          items: [
+            { text: "op-all", link: "/tool/op/op-all/index.md" },
+            { text: "op-by-search", link: "/tool/op/op-by-search/index.md" },
+          ],
+        },
+      ],
+    },
+  },
+});
---
+++ packages/web-site/docs/index.md
@@ -0,0 +1,43 @@
+---
+layout: home
+hero:
+  name: YiuTree
+  text: 树数据结构根据
+  tagline: 一个用于操作树的js库...
+#   image:
+#     src: /rubbish-tool-logo.png
+#     alt: Rubbish-tool
+  actions:
+    - theme: brand
+      text: 开始
+      link: /guide/index.md
+    - theme: alt
+      text: 在GitHub上查看
+      link: https://github.com/fidelyiu/yiu-tree-js
+
+# features:
+#   - icon: 🕒
+#     title: Date工具
+#     details: 格式化、取整...
+#   - icon: 💷
+#     title: Dom工具
+#     details: 父节点、匹配...
+#   - icon: ⛓
+#     title: 线段工具
+#     details: 相交、包含...
+#   - icon: 🍳
+#     title: 数组工具
+#     details: key索引...
+#   - icon: 🔟
+#     title: 数字工具
+#     details: 小数保留...
+#   - icon: 🧸
+#     title: 对象工具
+#     details: 字符串对比...
+#   - icon: 🎲
+#     title: 随机工具
+#     details: Id、数字...
+#   - icon: 🔠
+#     title: 字符串工具
+#     details: 首字母、尾删除...
+---
